997431f38bc473b8d05a60a55f9f540d233d4c2c
Nguyen Van Cao Ky <kynvche163260@fpt.edu.vn>
2022-09-28 19:38:03 +0700

    Fix something

--- src/main/webapp/assets/js/custom/apps/user-management/users/list/table-edited.js
+++ src/main/webapp/assets/js/custom/apps/user-management/users/list/table-edited.js
@@ -81,7 +81,7 @@ var KTUsersList = function () {
         processing: true,
         serverSide: true,
         order: [[1, "asc"]],
-        stateSave: false,
+        stateSave: true,
         select: {
           style: "multi",
           selector: 'td:first-child input[type="checkbox"]',
