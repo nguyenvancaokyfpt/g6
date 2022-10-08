a0f766d02c63cd050bd990dc41a1b109ef22ecb5
Nguyen Van Cao Ky <kynvche163260@fpt.edu.vn>
2022-10-08 22:07:54 +0700

    Class setting list

--- src/main/java/com/tss/dao/impl/ClassDaoImpl.java
+++ src/main/java/com/tss/dao/impl/ClassDaoImpl.java
@@ -19,7 +19,7 @@ public class ClassDaoImpl implements ClassDao {
         ResultSet resultSet = null;
         List<Classroom> classrooms = new ArrayList<Classroom>();
         if (connection != null) {
-            String sql = "select * from classroom";
+            String sql = "select class_id, class_code, combo_id, trainer_id, term_id, status.status_id, status.status_title, description from class, status where class.status_id = status.status_id;";
             Object[] params = {};
             try {
                 resultSet = BaseDao.execute(connection, preparedStatement, resultSet, sql, params);
@@ -35,7 +35,6 @@ public class ClassDaoImpl implements ClassDao {
                     classroom.setDescription(resultSet.getString("description"));
                     classrooms.add(classroom);
                 }
-                String sql = "select class_id, class_code, combo_id, trainer_id, term_id, status.status_id, status.status_title, description from classroom, status where classroom.status_id = status.status_id";
             } catch (SQLException e) {
                 e.printStackTrace();
             } finally {
@@ -47,8 +46,33 @@ public class ClassDaoImpl implements ClassDao {
 
     @Override
     public List<Classroom> findClassroomByStudent(Connection connection, int userId) throws SQLException {
-        // TODO Auto-generated method stub
-        return null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        List<Classroom> classrooms = new ArrayList<Classroom>();
+        if (connection != null) {
+            String sql = "select class.class_id, class.class_code, class.combo_id, class.trainer_id, class.term_id, class.status_id, status.status_title, class.description from class, status, student_class where class.status_id = status.status_id and class.class_id = student_class.class_id and student_class.student_id = ?;";
+            Object[] params = {userId};
+            try {
+                resultSet = BaseDao.execute(connection, preparedStatement, resultSet, sql, params);
+                while (resultSet.next()) {
+                    Classroom classroom = new Classroom();
+                    classroom.setClassId(resultSet.getInt("class_id"));
+                    classroom.setClassCode(resultSet.getString("class_code"));
+                    classroom.setComboId(resultSet.getInt("combo_id"));
+                    classroom.setTrainerId(resultSet.getInt("trainer_id"));
+                    classroom.setTermId(resultSet.getInt("term_id"));
+                    classroom.setStatusId(resultSet.getInt("status_id"));
+                    classroom.setStatusTitle(resultSet.getString("status_title"));
+                    classroom.setDescription(resultSet.getString("description"));
+                    classrooms.add(classroom);
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } finally {
+                BaseDao.closeResource(null, preparedStatement, resultSet);
+            }
+        }
+        return classrooms;
     }
 
     @Override
